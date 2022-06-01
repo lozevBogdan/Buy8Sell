48d82df79adcbd60b9cf08256029fc008866a977
lozev <lozev.bogdan@abv.bg>
2022-06-01 20:57:40 +0300

    Some data in DB for Users, products and picture!

--- src/main/java/com/example/sellbuy/service/impl/PictureServiceImpl.java
+++ src/main/java/com/example/sellbuy/service/impl/PictureServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PictureServiceImpl implements PictureService {
+
     private final PictureRepository pictureRepository;
 
     public PictureServiceImpl(PictureRepository pictureRepository) {
