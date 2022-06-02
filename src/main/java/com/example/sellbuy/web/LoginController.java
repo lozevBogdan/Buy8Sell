4b4847f9e95890786679ec9019dbeaf2ed8e6b10
lozev <lozev.bogdan@abv.bg>
2022-06-02 22:50:13 +0300

    LoginPage and registerPage

---
+++ src/main/java/com/example/sellbuy/web/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.sellbuy.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping
+    public String loginPage(){
+        return "loginPage";
+    }
+
+}
