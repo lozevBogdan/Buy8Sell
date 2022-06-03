b84c8186adf9a817575ee5b3d02614e8d00547ab
lozevBogdan <lozev.bogdan@abv.bg>
2022-06-03 17:05:06 +0300

    New navbar.

--- src/main/java/com/example/sellbuy/web/UserController.java
+++ src/main/java/com/example/sellbuy/web/UserController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class UserController {
 
     @GetMapping
     public String loginPage(){
         return "loginPage";
+    }
+
+
+    @GetMapping("/register")
+    public String registerPage(){
+        return "registerPage";
     }
 
 }
