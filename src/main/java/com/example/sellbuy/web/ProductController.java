cf45a2b0c91ce06a8ca46f7cd77eb95049178699
lozev <lozev.bogdan@abv.bg>
2022-06-07 23:44:13 +0300

    ProductController

--- src/main/java/com/example/sellbuy/web/ProductController.java
+++ src/main/java/com/example/sellbuy/web/ProductController.java
@@ -1,9 +1,15 @@
 package com.example.sellbuy.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
+
+    @GetMapping
+    public String productsPage(){
+        return "productsPage";
+    }
 
 
 
