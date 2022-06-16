de793626629097ba7abda7b9c65c5b7f49bfadaf
lozevBogdan <lozev.bogdan@abv.bg>
2022-06-16 17:20:54 +0300

    addPage - html

--- src/main/java/com/example/sellbuy/web/ProductController.java
+++ src/main/java/com/example/sellbuy/web/ProductController.java
@@ -2,13 +2,21 @@ package com.example.sellbuy.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("/products")
+    @GetMapping("/all")
     public String productsPage(){
         return "productsPage";
+    }
+
+
+    @GetMapping("/add")
+    public String allProducts(){
+        return "AddProductPage";
     }
 
 
