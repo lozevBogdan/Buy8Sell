420146dd59d842572c5e9db21e09c4662cb3c414
lozev <lozev.bogdan@abv.bg>
2022-06-27 23:23:30 +0300

    security

---
+++ src/main/java/com/example/sellbuy/security/CurrentUser.java
@@ -0,0 +1,38 @@
+package com.example.sellbuy.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class CurrentUser {
+
+    private Long id;
+
+    private String email;
+
+    public CurrentUser() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public CurrentUser setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CurrentUser setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public boolean isLogged(){
+        return (this.id != null && this.email != null);
+    }
+}
