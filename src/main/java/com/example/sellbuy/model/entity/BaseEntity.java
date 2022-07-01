0d3aa1db2e34129de2a41a2cba214e81dafd191f
lozevBogdan <lozev.bogdan@abv.bg>
2022-07-01 14:41:01 +0300

    add hashCode and equals with ID

--- src/main/java/com/example/sellbuy/model/entity/BaseEntity.java
+++ src/main/java/com/example/sellbuy/model/entity/BaseEntity.java
@@ -54,4 +54,18 @@ public abstract class BaseEntity {
         setModified(Instant.now());
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
 }
