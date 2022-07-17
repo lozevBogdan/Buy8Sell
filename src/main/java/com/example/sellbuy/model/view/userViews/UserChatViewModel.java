1948cdf0689eb07eeaafbbe827787be263592a34
lozevBogdan <lozev@students.softuni.bg>
2022-07-17 10:39:12 -0900

    UserChatViewModel used for rendering of messages.

---
+++ src/main/java/com/example/sellbuy/model/view/userViews/UserChatViewModel.java
@@ -0,0 +1,26 @@
+package com.example.sellbuy.model.view.userViews;
+
+public class UserChatViewModel {
+
+    private Long id;
+    private String fullName;
+
+    public UserChatViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
