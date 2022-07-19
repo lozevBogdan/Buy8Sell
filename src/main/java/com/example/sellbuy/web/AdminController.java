96f320c95fee46e343ba890b47ce2bd212e27daa
lozevBogdan <lozev.bogdan@abv.bg>
2022-07-19 11:34:43 +0300

    GetMapping users info.

--- src/main/java/com/example/sellbuy/web/AdminController.java
+++ src/main/java/com/example/sellbuy/web/AdminController.java
@@ -1,15 +1,29 @@
 package com.example.sellbuy.web;
 
+import com.example.sellbuy.model.view.userViews.UserInfoViewModel;
+import com.example.sellbuy.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/users")
-    public String allUsers(){
+    public String allUsers(Model model){
+
+        List<UserInfoViewModel> allUsersViewModels = this.userService.getAllUsers();
+        model.addAttribute("allUsersViewModels",allUsersViewModels);
         return "admin-all-users";
     }
 
