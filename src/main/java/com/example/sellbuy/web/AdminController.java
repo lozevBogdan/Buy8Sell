fb9346a0bd856a5ace5e57eb4c15b5d55ffd7cbe
lozevBogdan <lozev.bogdan@abv.bg>
2022-07-19 10:43:14 +0300

    Admin controller and template with all users table - basic skeleton.

---
+++ src/main/java/com/example/sellbuy/web/AdminController.java
@@ -0,0 +1,16 @@
+package com.example.sellbuy.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/users")
+    public String allUsers(){
+        return "admin-all-users";
+    }
+
+}
