f0a34408362a7e6522cb07728040a549d2e1bf87
lozevBogdan <lozev.bogdan@abv.bg>
2022-08-02 20:10:15 +0300

    DB initialization with Event listener.

--- src/main/java/com/example/sellbuy/init/DBInit.java
+++ src/main/java/com/example/sellbuy/init/DBInit.java
@@ -13,45 +13,11 @@ public class DBInit implements CommandLineRunner {
     public DBInit(ApplicationEventPublisher eventPublisher) {
         this.eventPublisher = eventPublisher;
     }
-
-
-
     @Override
     public void run(String... args) throws Exception {
 
         InitializationEvent initializationEvent = new InitializationEvent(DBInit.class);
         eventPublisher.publishEvent(initializationEvent);
-
     }
-//
-//    private final UserService userService;
-//    private final ProductService productService;
-//    private final PictureService pictureService;
-//    private final CategoryService categoryService;
-//    private final LocationService locationService;
-//
-//    public DBInit(UserService userService, ProductService productService,
-//                  PictureService pictureService,@Lazy CategoryService categoryService,
-//                  LocationService locationService) {
-//
-//        this.userService = userService;
-//        this.productService = productService;
-//        this.pictureService = pictureService;
-//        this.categoryService = categoryService;
-//        this.locationService = locationService;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//        this.locationService.initializeLocations();
-//        this.userService.initializeUsersAndRoles();
-//        this.pictureService.initializePictures();
-//        this.categoryService.initializedCategories();
-//        this.productService.initializeProducts();
-//
-//    }
-
-
 
 }
