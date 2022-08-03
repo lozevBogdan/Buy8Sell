d3aed11bcf3f66e7610572a4c18eb4510a86a8f7
lozevBogdan <lozev.bogdan@abv.bg>
2022-08-03 18:18:20 +0300

    ProductFavoriteViewModel with builder setter.

--- src/main/java/com/example/sellbuy/model/view/productViews/ProductFavoriteViewModel.java
+++ src/main/java/com/example/sellbuy/model/view/productViews/ProductFavoriteViewModel.java
@@ -10,43 +10,48 @@ public class ProductFavoriteViewModel {
     private BigDecimal price;
     private boolean isPromo;
 
-    public boolean isPromo() {
-        return isPromo;
-    }
-
-    public void setPromo(boolean promo) {
-        isPromo = promo;
-    }
-
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public ProductFavoriteViewModel setId(Long id) {
         this.id = id;
+        return this;
     }
 
     public String getPictureUrl() {
         return pictureUrl;
     }
 
-    public void setPictureUrl(String pictureUrl) {
+    public ProductFavoriteViewModel setPictureUrl(String pictureUrl) {
         this.pictureUrl = pictureUrl;
+        return this;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    public ProductFavoriteViewModel setTitle(String title) {
         this.title = title;
+        return this;
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public ProductFavoriteViewModel setPrice(BigDecimal price) {
         this.price = price;
+        return this;
+    }
+
+    public boolean isPromo() {
+        return isPromo;
+    }
+
+    public ProductFavoriteViewModel setPromo(boolean promo) {
+        isPromo = promo;
+        return this;
     }
 }
