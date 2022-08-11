56edac0f99a72d147272719dd35f9bcb47289c40
lozevBogdan <lozev.bogdan@abv.bg>
2022-08-11 20:44:58 +0300

    SellBuyApplication

--- src/main/java/com/example/sellbuy/SellBuyApplication.java
+++ src/main/java/com/example/sellbuy/SellBuyApplication.java
@@ -13,30 +13,7 @@ import java.lang.reflect.Field;
 public class SellBuyApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SellBuyApplication.class, args);
-
-
-
-//todo: IMPORTANT: implement REST fetch - almost done
-
-//todo: IMPORTANT: implement somewhere Exception handling (error handling)
-//   done     idea 1 - exeption for unauthorized user, try to edit product
-//      done     idea 2 -  error exception in case we wont to product with not exist id in DB - PRODUCT NOT FOUND EXCEPTION.
-
-//todo: IMPORTANT: intercetor - statistic for visiting
-
-//todo: IMPORTANT: implement Event -
-//                  idea1- listener for getting product info -> increase a views with listener
-//         idea2 - listeners for initializing DB. - DONE!
-
-// DONE todo: IMPORTANT: implement Schedule  -
-//                  idea1- schedule for products which not updated more than 30 days - DONE.
-//          idea2- rotate in every hour a different products on index page + Cache. - DONE!
-
-//todo: MAYBE add a new comments for approved by Admin, before their visualisation.
-
-
     }
 
 }
